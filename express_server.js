e0e26cd8ffc8711c99e7375d016aaef9d91fddfc
John <johnanthonylennie@gmail.com>
2017-06-08 21:46:58 +0000

    created URL Submission Form, implemented POST Requests, and Request Routing, also added generateRandomString function to express_server.js

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // fake shortened URLs to simulate a database
 var urlDatabase = [
   {
@@ -18,6 +21,11 @@ var urlDatabase = [
 
 // set the view engine to ejs
 app.set("view engine", "ejs")
+
+// add new URl page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 // all URLs page
 app.get("/urls", (req, res) => {
@@ -41,6 +49,19 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for( var i=0; i < 6; i++ )
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
