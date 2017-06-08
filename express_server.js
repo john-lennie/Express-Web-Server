16bfc51d93b1d139a8c1c4512fad7d5e5c2c6824
John <johnanthonylennie@gmail.com>
2017-06-08 19:53:31 +0000

    completed Template Engine - EJS asignment

--- express_server.js
+++ express_server.js
@@ -1,18 +1,44 @@
+// require and instantiate express
 var express = require("express");
+
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+// fake shortened URLs to simulate a database
+var urlDatabase = [
+  {
+    "id": "b2xVn2",
+    "url": "http://www.lighthouselabs.ca",
+  },
+  {
+    "id": "9sm5xK",
+    "url": "http://www.google.com"
+  }
+];
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+// set the view engine to ejs
+app.set("view engine", "ejs")
+
+// all URLs page
+app.get("/urls", (req, res) => {
+  // define  object as templateVars variable
+  let templateVars = { urls: urlDatabase };
+  // pass the object to the render method so we can make certain data available for the template
+  res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// single URL page given an id
+app.get("/urls/:id", (req, res) => {
+  // run through objects in database to see if given id is equal to an objects id, if it is define a new variable equal to fullURL
+  let fullURL = null;
+  urlDatabase.forEach(function(url) {
+    if (url.id == req.params.id) {
+      fullURL = url.url;
+    }
+  })
+  let templateVars = { shortURL: req.params.id, fullURL: fullURL };
+  // pass the object to the render method so we can make certain data available for the template
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
